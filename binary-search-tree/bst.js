eb3ffbf00a994d3ff4d9be3535fc1b28fd246078
MarLatte <86508134+marlatte@users.noreply.github.com>
2023-11-17 19:11:21 -0500

    Build basic tree

--- binary-search-tree/bst.js
+++ binary-search-tree/bst.js
@@ -6,13 +6,54 @@ function NodeFactory(data) {
 	};
 }
 
-function arrayGenerator(len) {
-	const output = [];
-	for (let i = 0; i < len; i++) {
-		output.push(Math.floor(Math.random() * 100));
+function TreeFactory(arr) {
+	const root = buildTree(arr);
+	return {
+		root,
+		printTree: () => prettyPrint(root),
+	};
+}
+
+function buildTree(arr) {
+	if (!arr.length) return null;
+	const mid = Math.floor(arr.length / 2);
+	const node = NodeFactory(arr[mid]);
+
+	node.left = buildTree(arr.slice(0, mid));
+	node.right = buildTree(arr.slice(mid + 1));
+
+	return node;
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+	if (node === null) {
+		return;
 	}
-	output.sort((a, b) => a - b);
-	return output;
+	if (node.right !== null) {
+		prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+	}
+	console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+	if (node.left !== null) {
+		prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+	}
+};
+
+function arrayGenerator(len) {
+	const output = new Set();
+	while (output.size < len) {
+		output.add(Math.floor(Math.random() * 100) + 1);
+	}
+	return [...output].sort((a, b) => a - b);
 }
 
 const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const testTree = TreeFactory(testArray);
+console.log('\nTest Tree from: ');
+console.log(testArray);
+testTree.printTree();
+
+const randomArray = arrayGenerator(11);
+const randomTree = TreeFactory(randomArray);
+console.log('\nRandom Tree from:');
+console.log(randomArray);
+randomTree.printTree();
